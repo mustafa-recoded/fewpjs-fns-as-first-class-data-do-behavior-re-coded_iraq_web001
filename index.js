99858c035f0b467a051abf179ada78ccbf735222
Mustafa Nawzat <mustafa@re-coded.com>
2020-05-31 11:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  const hhmm = parseInt(time);
+  
+  if (hhmm < 12) {
+    return ('Good Morning')
+  }else if (hhmm >= 12 && hhmm < 17) {
+    return ('Good Afternoon')
+  } else {
+    return ('Good Evening')
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
